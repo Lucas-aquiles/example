d30b07470cb8d08ac057b7a7e522877d7fa14ce6
lucas <you@example.com>
2022-04-25 13:40:14 -0300

    chn

--- src/db.js
+++ src/db.js
@@ -2,45 +2,30 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const {
+  DATABASE_URL,
+} = process.env;
 
-
-let sequelize =
-  process.env.NODE_ENV === "production"
-    ? new Sequelize({
-      database: DB_NAME,
-      dialect: "postgres",
-      host: DB_HOST,
-      port: 5432,
-      username: DB_USER,
-      password: DB_PASSWORD,
-      pool: {
-        max: 3,
-        min: 1,
-        idle: 10000,
-      },
-      dialectOptions: {
-        ssl: {
-          require: true,
-          // Ref.: https://github.com/brianc/node-postgres/issues/2009
-          rejectUnauthorized: false,
-        },
-        keepAlive: true,
-      },
-      ssl: true,
-    })
-    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/paises`, {
-      logging: false, // set to console.log to see the raw SQL queries
-      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-      //CONFIGURACION ADCIONAL
-
-
-    });
+const sequelize = new Sequelize(DATABASE_URL, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  //CONFIGURACION ADCIONAL
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
 
+const actArray = ["Observacion de atractivos de la naturaleza", "Observacion de fauna",
+  "Observacion de flora", "Caminata", "Ciclismo", "Cabalgata", "Buceo", "Descenso en rios",
+  "Pesca recreativa", "Paracaidismo", "Vuelo Parapente", "Recorridos por Bodegas",
+  "Visitas a Centros Comerciales", "Visitas guiadas al patrimonio historico ", "Rafting"]
 
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
@@ -70,4 +55,5 @@ Activity.belongsToMany(Country, { through: "intermedio" })
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  actArray,
 };
