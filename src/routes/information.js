c9487301f3b3f8786145ad8bf4f266b352bd9207
lucas <you@example.com>
2022-05-10 18:54:36 -0300

    c

--- src/routes/information.js
+++ src/routes/information.js
@@ -4,8 +4,9 @@ const server = require("express").Router();
 
 const allInformacion = require('../All/CHILD/AdidasChild.json')
 const allInfant = require('../All/INFANT/ADIDAS-INFANT0.json')
-
-
+const allMen = require('../All/MEN/AdidasMen0.json')
+const allUnisex = require('../All/UNISEX/ADIDAS-UNISEX.json')
+const allWomen = require('../All/WOMEN/ADIDAS-WOMEN0.json')
 
 server.get('/children', (req, res) => {
 
@@ -19,11 +20,24 @@ server.get('/infant', (req, res) => {
 
 })
 
-// server.get('/men', (req, res) => {
+server.get('/men', (req, res) => {
 
-//     res.status(200).json(allMen)
+    res.status(200).json(allMen)
 
-// })
+})
+
+server.get('/unisex', (req, res) => {
+
+    res.status(200).json(allUnisex)
+
+})
+
+
+server.get('/unisex', (req, res) => {
+
+    res.status(200).json(allWomen)
+
+})
 
 
 
