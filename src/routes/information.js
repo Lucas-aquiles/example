49ec1f303c08e289a3cb897a51ce81e51f07fc9b
lucas <you@example.com>
2022-05-10 18:14:19 -0300

    changes

--- src/routes/information.js
+++ src/routes/information.js
@@ -5,6 +5,7 @@ const { API, API_NAME } = process.env;
 const server = require("express").Router();
 // const API = "https://restcountries.com/v3/all"
 const allInformacion = require('../All/CHILD/infoNiños.json')
+const allInfant = require('../All/INFANT/ADIDAS-INFANT0.json')
 
 
 
@@ -13,13 +14,17 @@ const allInformacion = require('../All/CHILD/infoNiños.json')
 
 
 
-server.get('/', (req, res) => {
+server.get('/children', (req, res) => {
 
     res.status(200).json(allInformacion)
 
 })
 
+server.get('/infant', (req, res) => {
 
+    res.status(200).json(allInfant)
+
+})
 
 
 
