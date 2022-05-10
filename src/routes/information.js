57d734b08679ffce694686817a63efebd90329db
lucas <you@example.com>
2022-05-10 17:24:54 -0300

    dh

--- src/routes/information.js
+++ src/routes/information.js
@@ -5,11 +5,12 @@ const { API, API_NAME } = process.env;
 const server = require("express").Router();
 // const API = "https://restcountries.com/v3/all"
 const info = require('../AdidasMen0.json');
+const allInformacion = require('../All')
 
 
 server.get('/', (req, res) => {
 
-    res.status(200).json(info)
+    res.status(200).json(allInformacion)
 
 })
 
