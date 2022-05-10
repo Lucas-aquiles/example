15a6400b22ae98a04299f3af2efb21e9d0611d86
lucas <you@example.com>
2022-05-10 18:42:39 -0300

    ch

--- src/routes/information.js
+++ src/routes/information.js
@@ -1,33 +1,31 @@
-// const express = require('express');
-// require('dotenv').config();
-// const axios = require('axios');
-// const { API, API_NAME } = process.env;
-// const server = require("express").Router();
+const express = require('express');
+require('dotenv').config();
+const server = require("express").Router();
 
-// const allInformacion = require('../All/CHILD/infoNiños.json')
-// const allInfant = require('../All/INFANT/ADIDAS-INFANT0.json')
+const allInformacion = require('../All/CHILD/infoNiños.json')
+const allInfant = require('../All/INFANT/ADIDAS-INFANT0.json')
 
 
 
-// server.get('/children', (req, res) => {
+server.get('/children', (req, res) => {
 
-//     res.status(200).json(allInformacion)
+    res.status(200).json(allInformacion)
+
+})
+
+server.get('/infant', (req, res) => {
+
+    res.status(200).json(allInfant)
+
+})
+
+// server.get('/men', (req, res) => {
+
+//     res.status(200).json(allMen)
 
 // })
 
-// server.get('/infant', (req, res) => {
-
-//     res.status(200).json(allInfant)
-
-// })
-
-// // server.get('/men', (req, res) => {
-
-// //     res.status(200).json(allMen)
-
-// // })
 
 
 
-
-// module.exports = server;
+module.exports = server;
